f00553773e70b46e3aaaa67da3fc0e5c728d8a56
LaoCC <fazo@qq.com>
2022-02-25 20:28:50 +0800

    d

--- controllers/Debug.php
+++ controllers/Debug.php
@@ -436,4 +436,28 @@ class Debug extends Controller
     }
 
 
+    public function cacheGet()
+    {
+        $data = $this->getConfig()->allConfig();
+        unset($data['mime'], $data['state']);
+        $this->assign('data', $data);
+    }
+
+    public function opcacheGet()
+    {
+
+    }
+
+    public function resourceAjax()
+    {
+        $this->_redis->set('resourceRand', time() + mt_rand());
+        return ['success' => 1, 'message' => "重置成功"];
+    }
+
+    public function flushAjax($all)
+    {
+        $this->getConfig()->flush(intval($all));
+        return ['success' => 1, 'message' => "清空成功"];
+    }
+
 }
